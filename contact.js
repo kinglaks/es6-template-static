4c30980d6b61831561cf91951780ecff0cb4f9c4
Lakshan Gunajothy <lakshangunajothy@Lakshans-MacBook-Pro.local>
2020-12-10 21:24:44 -0500

    forms

--- contact.js
+++ contact.js
@@ -7,6 +7,25 @@ module.exports = class extends Page {
     render(sPage) {
         return `
         ${this.requireMarked('_pages/contact.md')}
+        <form action="http://localhost:3002/send" method="POST">
+            <div class="form-group">
+                <!-- give inputs the class form-control -->
+                <label>Name: <input name="name" placeholder="name" class="form-control" /></label>
+            </div>
+            <div class="form-group">
+                <label>Email:<input name="email" placeholder="email" type="email" class="form-control" /></label>
+            </div>
+            <div class="form-group">
+                <label>Phone Number:<input name="phone" placeholder="telephone" type="tel"
+                        class="form-control" /></label>
+            </div>
+            <div class="form-group">
+                <label>Message:<textarea name="message" placeholder="type your message here"
+                        class="form-control"></textarea></label>
+            </div>
+            <button type="submit">Send Message</button>
+
+        </form>
         `;
     }
 }
